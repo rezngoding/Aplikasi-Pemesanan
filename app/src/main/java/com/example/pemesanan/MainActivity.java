884dfdf42a8f756c25908775cf1e1614acac7b77
rezngoding <12250115572@student.uin-suska.ac.id>
2024-03-08 17:38:29 +0700

    percobaan

--- app/src/main/java/com/example/pemesanan/MainActivity.java
+++ app/src/main/java/com/example/pemesanan/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.pemesanan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+private LinearLayout    layout1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout1=findViewById(R.id.sepatu);
+        layout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ShoesActivity.class));
+            }
+        });
+
+
+
     }
 }
